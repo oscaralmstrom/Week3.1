7e5fbb24038a4b4ecda381a43bab5ad687a40015
oscaralmstrom <42993328+oscaralmstrom@users.noreply.github.com>
2018-09-19 19:05:05 +0200

    Create

---
+++ src/exercises/Ex1LoopPuzzlers.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import static java.lang.System.out;
+
+/*
+ *  Some puzzlers
+ *  General improvement of programming skills
+ *
+ *  See:
+ *  - LoopPuzzler
+ */
+public class Ex1LoopPuzzlers {
+
+    public static void main(String[] args) {
+        new Ex1LoopPuzzlers().program();
+    }
+
+
+    void program() {
+        // Write solution (for loops) directly here
+
+    }
+}
